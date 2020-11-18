538d0f708e3c1cc6645a5f94795878db0933730f
LuK1337 <priv.luk@gmail.com>
2020-11-18 10:52:15 +0100

    Revert "hwc2: Restore old brightness control"
    This reverts commit 201c2fd0804897be8c6120a23fa1b7cc139b7585.

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -3339,7 +3339,7 @@ int32_t HWCSession::GetDisplayCapabilities(hwc2_device_t *device, hwc2_display_t
   if (!outCapabilities) {
     *outNumCapabilities = 0;
     if (isBuiltin) {
-      *outNumCapabilities = 3;
+      *outNumCapabilities = 4;
     }
     return HWC2_ERROR_NONE;
   } else {
@@ -3352,6 +3352,7 @@ int32_t HWCSession::GetDisplayCapabilities(hwc2_device_t *device, hwc2_display_t
       if (has_doze_support) {
         outCapabilities[index++] = HWC2_DISPLAY_CAPABILITY_DOZE;
       }
+      outCapabilities[index++] = HWC2_DISPLAY_CAPABILITY_BRIGHTNESS;
       outCapabilities[index++] = UINT32(HwcDisplayCapability::PROTECTED_CONTENTS);
       *outNumCapabilities = index;
     }
@@ -3421,7 +3422,7 @@ int32_t HWCSession::GetDisplayBrightnessSupport(hwc2_device_t *device, hwc2_disp
     DLOGE("Expected valid hwc_display");
     return HWC2_ERROR_BAD_PARAMETER;
   }
-  *outSupport = false;
+  *outSupport = (hwc_display->GetDisplayClass() == DISPLAY_CLASS_BUILTIN);
   return HWC2_ERROR_NONE;
 }
 
